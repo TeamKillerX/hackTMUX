0cb4d5b9e9c9800b5904b86622ee6697ada6cd62
xtsea <90479255+xtsea@users.noreply.github.com>
2024-07-28 16:34:31 +0700

    _

--- chatgpt.py
+++ chatgpt.py
@@ -7,18 +7,19 @@ async def main():
     parser.add_argument('--ask', type=str, help='A question to be asked')
 
     args = parser.parse_args()
-    url = "https://randydev-ryuzaki-api.hf.space/ryuzaki/chatgpt-old"
-    params = {"query": args.ask}
-    check_response = await AsyicXSearcher.search(
-        url,
-        post=True,
-        re_json=True,
-        json=params
-    )
+
     if args.ask:
+        url = "https://randydev-ryuzaki-api.hf.space/ryuzaki/chatgpt-old"
+        params = {"query": args.ask}
+        check_response = await AsyicXSearcher.search(
+            url,
+            post=True,
+            re_json=True,
+            json=params
+        )
         print(check_response["randydev"]["message"])
     else:
         print("No question was asked.")
 
 if __name__ == "__main__":
-    await asyncio.run(main())
+    asyncio.run(main())
