df9e5e77c903d4764151c636c1b162431b1d2551
Randy Dev <90479255+xtsea@users.noreply.github.com>
2024-08-28 20:59:30 +0700

    _

--- tools.py
+++ tools.py
@@ -1,19 +1,22 @@
 import typer
+import asyncio
 from reqs import AsyicXSearcher
 
 app = typer.Typer()
 
 @app.command()
-async def chat(message: str):
-    url = "https://akeno.randydev.my.id/ryuzaki/chatgpt-old"
-    params = {"query": message}
-    check_response = await AsyicXSearcher.search(
-        url,
-        post=True,
-        re_json=True,
-        json=params
-    )
-    print(check_response["randydev"]["message"])
+def chat(message: str):
+    async def _chat():
+        url = "https://akeno.randydev.my.id/ryuzaki/chatgpt-old"
+        params = {"query": message}
+        check_response = await AsyicXSearcher.search(
+            url,
+            post=True,
+            re_json=True,
+            json=params
+        )
+        print(check_response["randydev"]["message"])
+    asyncio.run(_chat())
 
 if __name__ == "__main__":
     app()
